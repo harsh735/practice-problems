ed3d1aadc5ccfab20db89b1a4836c26880ba1c49
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-06-12 10:20:17 +0530

    Time: 8 ms (77.96%), Space: 11.1 MB (10.65%) - LeetHub

--- two-sum/two-sum.cpp
+++ two-sum/two-sum.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         map<int,int> m;
+        int diff;
         vector<int> v;
         for(int i=0;i<nums.size();i++)
         {
-            if(m.find(target-nums[i])!=m.end())
+            diff = target - nums[i];
+            if(m.find(diff)!=m.end() && m.find(diff)-> second != i)
             {
-                v.push_back(m[target-nums[i]]);
+                v.push_back(m[diff]);
                 v.push_back(i);
                 return v;
             }
