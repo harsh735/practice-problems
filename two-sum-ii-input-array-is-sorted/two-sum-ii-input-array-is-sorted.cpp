171fa7a41f5d452cd14fe39a176e2efcfeb08e87
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-06-12 11:05:26 +0530

    Update two-sum-ii-input-array-is-sorted.cpp

--- two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> v;
         int i = 0, j = numbers.size()-1;
         while(i<j)
         {
             int sum = numbers[i] + numbers[j];
             if(sum == target) 
             {
-               return {i+1,j+1};
+                v[0] = i+1;
+                v[1] = j+1;
             }
             if(sum >target)
             {
@@ -18,6 +20,6 @@ public:
                 i++;
             }
         }
-        return { };
+        return v;
     }
 };
