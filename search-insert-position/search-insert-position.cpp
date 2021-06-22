4afdea44a0067e7ab4b291faa56ca21a4a6c8d63
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-06-22 19:10:32 +0530

    Time: 4 ms (82.61%), Space: 9.7 MB (52.81%) - LeetHub

--- search-insert-position/search-insert-position.cpp
+++ search-insert-position/search-insert-position.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int i,n = nums.size();
-        int j = nums.size() - 1;
         for(i = 0; i < n; i++)
         {
             if(nums[i] >= target){return i;}
         }
-        return nums.size();
+        return i;
     }
 };
