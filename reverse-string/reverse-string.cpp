5e7c6360af20a6eba04b11fac8aba05e49235674
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-09-13 02:20:31 +0530

    Time: 26 ms (30.46%), Space: 23.3 MB (41.19%) - LeetHub

---
+++ reverse-string/reverse-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    void revString(vector<char>&s, int start, int end){
+        
+        //najajajajajajaa
+        
+        
+        if(start > end){
+            return;
+        }
+        //storing current letter in a temp char
+        char temp = s[start];
+        s[start] = s[end]; //storing last letter in first pos
+        s[end] = temp; //storing first letter in last pos
+        revString(s, start+1, end-1); //recursive call
+    
+    }
+    
+    
+    void reverseString(vector<char>& s) {
+        revString(s,0,s.size() - 1);
+    }
+};
