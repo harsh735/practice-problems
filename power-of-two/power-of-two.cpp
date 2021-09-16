09e9709b5417d5f78d25956b005fb6f0db27698f
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-09-17 01:25:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (21.54%) - LeetHub

---
+++ power-of-two/power-of-two.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        
+        //I DID NOT JUST DO THIS??????????????????????????????????????????????
+        //I FEEL I GAINED A SUPERPOWER WHAT 
+        
+        if(n == 0)
+            return false;
+        
+        if(n == 1)
+            return true;
+        
+         while(n%2 == 0){
+            n = n/2;
+            if(n==1)return true;
+        }
+        return false;
+        
+    }
+};
