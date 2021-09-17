c3283f5dff326ddab5c54aea9986cb03bec044ae
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-09-18 03:07:38 +0530

    Time: 4 ms (47.57%), Space: 8.4 MB (40.21%) - LeetHub

--- maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
+++ maximum-ascending-subarray-sum/maximum-ascending-subarray-sum.cpp
@@ -5,13 +5,15 @@ public:
         int ans=nums[0];
         int curr=nums[0];
         for(int i =1; i<nums.size(); i++){
-            if(nums[i] <= nums[i-1]){
-                curr = nums[i];
+            if(nums[i-1] < nums[i]){
+                curr += nums[i];
             }
             else{
-                curr+=nums[i];
-                ans = max(ans,curr);
+                curr  = nums[i];
             }
+            
+            ans = max(ans,curr);
+
         }
         
         return ans;
