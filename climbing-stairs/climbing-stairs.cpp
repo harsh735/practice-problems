36b8468434786eff9715bbdbfcf63d51491773be
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-10-04 22:59:30 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (14.27%) - LeetHub

---
+++ climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        unordered_map<int, int> memo = {};
+        return totalWays(0,n, memo);    
+    }
+    
+    int totalWays(int currentStair, int targetStair, unordered_map<int,int> &memo){
+        
+        if(currentStair == targetStair) return 1;
+        
+        else if(currentStair > targetStair) return 0;
+        
+        
+        int currentKey = currentStair;
+        
+        for(auto it: memo){
+            if(it.first == currentKey){
+                return memo[currentKey];
+            }
+        }
+        
+        int oneStep = totalWays(currentStair+1, targetStair, memo);
+        int twoStep = totalWays(currentStair+2, targetStair, memo);
+        
+        memo[currentKey] = oneStep + twoStep;
+        return oneStep + twoStep;
+    }
+    
+    
+};
