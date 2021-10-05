027e3717a34409b86ae12a8ea7335584bbda0365
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-10-05 22:26:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (14.29%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int tribonacci(int n) {
+        //(ღ˘⌣˘ღ) BOBOBOBOBOBOBOBO
+        unordered_map<int, int> memo = {};
+        return tribo(n,memo);
+    }
+    
+    int tribo(int n, unordered_map<int, int> &memo){
         
-        //(ღ˘⌣˘ღ)
+        if(n == 1 || n == 0) return n;
+        else if(n == 2) return 1;
+        int currentKey = n;
         
-        int dp[38] = {0};
-        dp[0]=0, dp[1]=1, dp[2]=1; 
-        
-        if(n<3) return dp[n];
-        
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        for(auto it: memo){
+            if(it.first == currentKey) return memo[currentKey];
         }
-        return dp[n];
+        
+        int oneStep = tribo(n-1,memo);
+        int twoStep = tribo(n-2,memo);
+        int threeStep = tribo(n-3, memo);
+        
+        memo[currentKey] = oneStep + twoStep + threeStep;
+        return memo[currentKey];
     }
 };
