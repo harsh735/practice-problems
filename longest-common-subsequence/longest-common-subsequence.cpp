66e26b630d62ea19b976293a0860b414044b94d4
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-10-12 03:30:59 +0530

    Time: 1224 ms (5.18%), Space: 102.4 MB (6.08%) - LeetHub

--- longest-common-subsequence/longest-common-subsequence.cpp
+++ longest-common-subsequence/longest-common-subsequence.cpp
@@ -5,21 +5,19 @@ public:
         //◉_◉
         
         map<pair<int,int>,int> memo;
-        int n = text1.size();
-        int m = text2.size();
-        return solve(text1,text2,n,m,0,0,memo);
+        return solve(text1,text2,0,0,memo);
     }
     
-    int solve(string& text1, string& text2, int n, int m, int i, int j, map<pair<int,int>,int> &memo){
+    int solve(string& text1, string& text2, int i, int j, map<pair<int,int>,int> &memo){
        
-        if(i == n || j == m) return 0;
+        if(i >= text1.size() || j >= text2.size()) return 0;
         
         if(memo.count(make_pair(i,j)) == 1) return memo[make_pair(i,j)];
         
         if(text1[i] == text2[j]) 
-            return memo[make_pair(i,j)] = 1 + solve(text1,text2,n,m,i+1,j+1,memo);
+            return memo[make_pair(i,j)] = 1 + solve(text1,text2,i+1,j+1,memo);
             
         else 
-            return memo[make_pair(i,j)] = max(solve(text1,text2,n,m,i+1,j,memo),solve(text1,text2,n,m,i,j+1,memo)); 
+            return memo[make_pair(i,j)] = max(solve(text1,text2,i+1,j,memo),solve(text1,text2,i,j+1,memo)); 
     }
 };
