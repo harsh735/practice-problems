840624e646627105cecdf3a7826e5caf96154db4
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-10-12 01:52:14 +0530

    Time: 12 ms (77.86%), Space: 16 MB (11.29%) - LeetHub

--- majority-element-ii/majority-element-ii.cpp
+++ majority-element-ii/majority-element-ii.cpp
@@ -2,23 +2,17 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        if(nums.size() == 1){
-            return nums;
-        }
+        // ~o~
         
-        unordered_map<int, int> newNums;
+        if(nums.size() == 1) return nums;
+        unordered_map<int,int> newNums;
         vector<int> ans;
-        for(int i =0; i<nums.size(); i++){
+        for(int i = 0; i < nums.size(); ++i){
             newNums[nums[i]]++;
         }
-        
         for(auto it: newNums){
-            
-            if(it.second > (nums.size()/3)){
-                ans.push_back(it.first);
-            }
+            if(it.second > (nums.size() / 3)) ans.push_back(it.first);
         }
         return ans;
-        
     }
 };
