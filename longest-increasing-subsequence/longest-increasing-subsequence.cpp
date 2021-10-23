fc289291d44a777b6b4bda6cd7f03ccdfa47ed6e
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-10-23 17:24:08 +0530

    Time: 1556 ms (5.00%), Space: 40.7 MB (5.02%) - LeetHub

---
+++ longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int memo[2600][2600];
+    int lengthOfLIS(vector<int>& nums) {
+        
+        // SOS !!!
+        
+        memset(memo,-1,sizeof(memo));
+        if(nums.size() == 0) return 0;
+        return solve(nums,0,-1);
+    }
+    
+    int solve(vector<int>&nums, int currIdx, int prev) {
+        
+        if(currIdx >= nums.size()) return 0;
+        
+        if(prev != -1 && memo[currIdx][prev] != -1) return memo[currIdx][prev];
+        int consider = 0;
+        if(prev == -1 || nums[currIdx] > nums[prev]) 
+           consider = 1 + solve(nums,currIdx + 1,currIdx);
+        
+        
+        int notConsider = solve(nums,currIdx + 1,prev);
+        if(prev != -1) memo[currIdx][prev] = max(consider,notConsider);
+        return max(consider,notConsider);
+    }
+};
