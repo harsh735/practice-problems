f7191644dd68965ae4dc5fbd1e52b4de175737c9
harsh735 <53695605+harsh735@users.noreply.github.com>
2021-11-20 00:04:16 +0530

    Update search-in-rotated-sorted-array-ii.cpp

--- search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -2,8 +2,22 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         
-        for(int i = 0; i<nums.size(); i++) {
-            if(nums[i] == target) return true;
+        int start = 0,end = nums.size() - 1;
+        
+        while(start <= end) {
+            
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target) return true;
+            
+            if (nums[start]<nums[mid]) {
+                if (target < nums[start] || target > nums[mid]) start = mid+1;
+                else end = mid-1;
+            }
+            else if (nums[start] > nums[mid]) {
+                if (target < nums[mid]  || target > nums[end]) end = mid-1;
+                else start =  mid+1;
+            }
+            else  start++;
         }
         return false;
     }
