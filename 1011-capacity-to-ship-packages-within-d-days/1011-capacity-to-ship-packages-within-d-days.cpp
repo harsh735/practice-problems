1fb7571b6a69b45265fd53f73edd657ae9ee6de0
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-01-31 02:58:12 +0530

    Time: 57 ms (67.48%), Space: 26 MB (94.69%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+     //application of split array largest sum, ditto copy
+     bool isValid(vector<int>& weights, int days, int mid) {
+        int sum = 0;
+        int d = 1;
+         
+        for(int i = 0; i < weights.size(); i++) {
+            sum += weights[i];
+            if(sum > mid) {
+                d++;
+                sum = weights[i];
+            }
+            if(d > days)
+                return false;
+        }
+        
+        return true;
+    }
+    
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+        int maxi = 0;
+        int sum = 0;
+        for(int i = 0; i < weights.size(); i++) {
+            sum += weights[i];
+            maxi = max(maxi,weights[i]);
+        }
+        
+        if(weights.size() == days) return maxi;
+        
+        int start = maxi, end = sum;
+        int ans = 0;
+        
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if(isValid(weights, days, mid) == true) {
+                ans = mid;
+                end = mid - 1;
+            } 
+            else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+     }
+};
