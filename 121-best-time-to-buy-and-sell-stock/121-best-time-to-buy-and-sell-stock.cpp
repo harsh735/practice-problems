d3d4e0f0c4cae89f0f77c9274c304a8779d825cb
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-02-01 22:23:26 +0530

    Time: 1705 ms (5.14%), Space: 352.6 MB (5.68%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        vector<vector<vector<int>>> memo(prices.size() + 1, vector<vector<int>>(3, vector<int>(3,-1)));
+        return solve(prices,0,1,1,memo);
+    }
+    
+    int solve(vector<int>&prices, int currIdx, int canBuy, int transactionCount, vector<vector<vector<int>>>&memo) {
+        
+        if(currIdx >= prices.size() || transactionCount <= 0) return 0;
+        
+        if(memo[currIdx][canBuy][transactionCount] != -1) return memo[currIdx][canBuy][transactionCount];
+                
+        
+        int idle = solve(prices,currIdx + 1,canBuy,transactionCount,memo);
+        if(canBuy) {
+            int buy = -prices[currIdx] + solve(prices,currIdx + 1,0,transactionCount,memo);
+            return memo[currIdx][canBuy][transactionCount] = max(idle,buy);
+        } 
+        
+        else {
+            int sell = prices[currIdx] + solve(prices,currIdx + 1,1, transactionCount - 1,memo);
+            return memo[currIdx][canBuy][transactionCount] = max(idle,sell);
+        }
+            
+    }
+};
