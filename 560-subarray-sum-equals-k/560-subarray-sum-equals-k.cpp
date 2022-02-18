a1a8fe7d0d33733bf711da2b3f9f7d35637fd13c
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-02-18 13:43:45 +0530

    Time: 73 ms (88.78%), Space: 35.9 MB (96.88%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -4,10 +4,12 @@ public:
         
         int ans = 0,sum = 0;
         unordered_map<int,int> mp;
-        mp[0] = 1;
         for(int i = 0; i<nums.size(); i++){
             sum += nums[i];
             int key = sum - k;
+            
+            if(sum == k)
+                ans++;
             
             if(mp.find(key) != mp.end()){
                 ans += mp[key];
