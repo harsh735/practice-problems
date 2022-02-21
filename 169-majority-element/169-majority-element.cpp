9669b00c8c7aa0a848174e7c0b03d4def4a39a70
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-02-21 12:00:08 +0530

    Time: 54 ms (5.23%), Space: 19.7 MB (52.27%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {    
+        
+        int ans = 0;
+        int freq = 0;
+        for(int i = 0; i<nums.size(); i++){
+            
+            if(freq == 0)
+                ans = nums[i];
+            
+            if(ans == nums[i])
+                freq++;
+            
+            if(ans != nums[i])
+                freq--;
+            
+           
+        }
+        return ans;
+    }
+};
+
+
+/*
+
+//TC -> O(N)
+        //SC -> O(N)
+        unordered_map<int , int> newNums;
+        
+        for(int i = 0; i<nums.size(); i++){
+            newNums[nums[i]]++;
+        }
+                               
+        for(auto it: newNums){
+            if(it.second > (nums.size()/2)){
+                return it.first;
+            }
+        }
+        
+        return 0;
+
+
+*/
