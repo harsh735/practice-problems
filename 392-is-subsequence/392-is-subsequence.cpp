9952e9c81b36f5ad4e7f33e109db1f8cdfba2302
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-03-02 20:28:25 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.59%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int i = 0,j=0;
+        while(i < t.size() && j < s.size()){
+            
+            if(s[j] == t[i])
+                j++;
+            
+            i++;
+        }
+        if(j == s.size())
+            return true;
+        return false;
+    }
+};
