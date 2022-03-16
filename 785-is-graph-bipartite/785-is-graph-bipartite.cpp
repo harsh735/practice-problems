be9caee3f08edd910fd30d76db079252b53b42c7
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-03-16 20:24:08 +0530

    Time: 20 ms (96.46%), Space: 14.2 MB (19.42%) - LeetHub

---
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        
+        int n = graph.size();
+        int color[n];
+        memset(color, -1, sizeof(color));
+        for(int i = 0; i<n; i++){
+            if(color[i] == -1){
+                if(!bfs(i,graph,color))
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    bool bfs(int node, vector<vector<int>>& graph, int color[]){
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int curr = q.front();
+            q.pop();
+            
+            for(auto it: graph[curr]){
+                if(color[it] == -1){
+                    color[it] = 1 - color[curr];
+                    q.push(it);
+                }
+                else if(color[it] == color[curr])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
