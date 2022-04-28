f0047018c65a6688396d51b7eea41508cd9164ef
harsh735 <53695605+harsh735@users.noreply.github.com>
2022-04-28 13:18:16 +0530

    Time: 48 ms (82.40%), Space: 16.2 MB (48.16%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+      int longestIncreasingPath(vector<vector<int>>& matrix) {
+          
+        int m=matrix.size();
+        int n=matrix[0].size();
+        
+        vector<vector<int>> memo(m+1,vector<int>(n+1,-1));
+        int ans=INT_MIN;
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                int temp=solve(matrix,i,j,m,n,-1,memo);
+                ans = max(ans,temp);
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+    
+    int solve(vector<vector<int>>& matrix,int currRow,int currCol,int m,int n,int prev,vector<vector<int>> &memo) {
+        
+        if(currRow < 0 || currCol< 0 || currRow >= m || currCol >= n || matrix[currRow][currCol] <= prev) return 0;
+        
+        if(memo[currRow][currCol] != -1) return memo[currRow][currCol];
+        
+        int left = 1 + solve(matrix,currRow,currCol-1,m,n,matrix[currRow][currCol],memo);
+        int up = 1 + solve(matrix,currRow - 1,currCol,m,n,matrix[currRow][currCol],memo);
+        int right = 1 + solve(matrix,currRow,currCol+1,m,n,matrix[currRow][currCol],memo);
+        int down = 1 + solve(matrix,currRow+1,currCol,m,n,matrix[currRow][currCol],memo);
+    
+        return memo[currRow][currCol] = max(left,max(up,max(right,down)));
+    }
+  
+};
